e871e7d04a06b5d3d24a2b19003f453bd2ef7f5c
Leonardo Menezes <leocarrerecm@gmail.com>
2022-06-22 15:50:00 -0300

    Fixes order test

--- orders/src/routes/__test__/index.test.ts
+++ orders/src/routes/__test__/index.test.ts
@@ -15,7 +15,7 @@ const buildTicket = async () => {
   return ticket;
 };
 
-it("fetches orders for a particular user", async () => {
+it("fetches orders for an particular user", async () => {
   // Create three tickets
   const ticketOne = await buildTicket();
   const ticketTwo = await buildTicket();
@@ -23,9 +23,7 @@ it("fetches orders for a particular user", async () => {
 
   const userOne = global.signin();
   const userTwo = global.signin();
-
   // Create one order as User #1
-
   await request(app)
     .post("/api/orders")
     .set("Cookie", userOne)
@@ -33,13 +31,11 @@ it("fetches orders for a particular user", async () => {
     .expect(201);
 
   // Create two orders as User #2
-
   const { body: orderOne } = await request(app)
     .post("/api/orders")
     .set("Cookie", userTwo)
     .send({ ticketId: ticketTwo.id })
     .expect(201);
-
   const { body: orderTwo } = await request(app)
     .post("/api/orders")
     .set("Cookie", userTwo)
@@ -51,11 +47,11 @@ it("fetches orders for a particular user", async () => {
     .get("/api/orders")
     .set("Cookie", userTwo)
     .expect(200);
-  // Make sure we only got orders for User #2
 
+  // Make sure we only got the orders for User #2
   expect(response.body.length).toEqual(2);
   expect(response.body[0].id).toEqual(orderOne.id);
   expect(response.body[1].id).toEqual(orderTwo.id);
-  expect(response.body[0].ticket).toEqual(ticketTwo.id);
-  expect(response.body[1].ticket).toEqual(ticketThree.id);
+  expect(response.body[0].ticket.id).toEqual(ticketTwo.id);
+  expect(response.body[1].ticket.id).toEqual(ticketThree.id);
 });
